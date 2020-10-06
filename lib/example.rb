f400e7aace99e9b8feef68997be0e5b19fc4118c
YOUR_GITHUB_USER_NAME <YOUR_GITHUB_EMAIL_ADDRESS>
2020-10-05 20:49:07 -0400

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,12 +1,15 @@
 # Use this space to code along with the readme
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+def NBA(phrase = "Hello World!")
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+end
+
+NBA("Alon")
